9988a9d18546c0c043793a6f036804d542258fcb
Mouneeta <71824279+Mouneeta@users.noreply.github.com>
2023-01-28 07:58:09 +0600

    Add files via upload
    second

---
+++ recommendation-system--main/backend/app.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+items = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           product_name = list(popular_df['Product-Title'].values),
+                           company=list(popular_df['Product-Company'].values),
+                           image=list(popular_df['Image-URL-S'].values),
+                           votes=list(popular_df['num_ratings'].values),
+                           rating=list(popular_df['avg_rating'].values)
+                           )
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = items[items['Product-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Product-Title')['Product-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Product-Title')['Product-Company'].values))
+        item.extend(list(temp_df.drop_duplicates('Product-Title')['Image-URL-S'].values))
+
+        data.append(item)
+
+    print(data)
+
+    return render_template('recommend.html',data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
